1f263e556460120f8a64e05c2d3a313edacf6fba
Seb <sebbird95@gmail.com>
2021-04-19 23:23:15 +0100

    add basic task counter

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,24 +5,29 @@ const Tasks = ({ tasks, handleDelete, handleComplete }) => {
     return <p>No tasks yet!</p>;
   }
 
-  return tasks.map(({ name, id, completed }) => {
-    return (
-      <>
-        <div className="tasks">
-          <p
-            key={name}
-            onClick={() => handleComplete(id)}
-            className={completed ? "striked" : ""}
-          >
-            {name}
-          </p>
-          <button className="delete" onClick={() => handleDelete(id)}>
-            x
-          </button>
-        </div>
-      </>
-    );
-  });
+  return (
+    <p>
+      Total tasks: {tasks.length}
+      {tasks.map(({ name, id, completed }) => {
+        return (
+          <>
+            <div className="tasks">
+              <p
+                key={name}
+                onClick={() => handleComplete(id)}
+                className={completed ? "striked" : ""}
+              >
+                {name}
+              </p>
+              <button className="delete" onClick={() => handleDelete(id)}>
+                x
+              </button>
+            </div>
+          </>
+        );
+      })}
+    </p>
+  );
 };
 
 export default Tasks;
